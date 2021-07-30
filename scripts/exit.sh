4186a8c3d0c81a4f6b9d172fe31e23897c9e9cb5
giuseppe <capassog97@gmail.com>
2021-07-30 14:38:57 +0200

    Added exit script

---
+++ scripts/exit.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env sh
+
+# CMD
+DMENU="dmenu -i" # -i case insensity
+LOCKSCREEN="betterlockscreen -l"
+POWEROFF="systemctl poweroff"
+REBOOT="systemctl reboot"
+
+# options
+declare -a options=(
+  "Poweroff"
+  "Logout"
+  "Reboot"
+  "Lock"
+  "Quit"
+)
+
+# look up what managers are used (makes it more dynamic)
+declare -a MANAGERS
+  while IFS= read -r manager
+  do
+    MANAGERS+=("${manager,,}")
+  done < <(grep 'Name' /usr/share/xsessions/*.desktop | awk -F"=" '{print $2}')
+
+
+choice=$(printf "%s\n" "${options[@]}"| $DMENU -p "Exit menu:")
+
+case "$choice" in
+  'Poweroff')
+    if [ $(echo -e 'No\nYes' | $DMENU -p "${choice}?" ) == "Yes" ]; then
+      $POWEROFF
+    fi
+    ;;
+  'Reboot')
+    if [ $(echo -e 'No\nYes' | $DMENU -p "${choice}?") == "Yes" ]; then
+      $REBOOT
+    fi
+    ;;
+  'Lock')
+    $LOCKSCREEN
+    ;;
+  'Logout')
+    if [ $(echo -e 'No\nYes' | $DMENU -p "${choice}?") == "Yes" ]; then
+      for manager in "${MANAGERS[@]}"; do 
+        killall "${manager}" || output "Process ${manager} was not running."
+      done
+    fi
+    ;;
+  'Quit')
+    exit 1
+    ;;
+esac
